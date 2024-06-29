117b97edb58ead181477b205d9403a7f61276552
SampathVinay2003 <100464051+SampathVinay2003@users.noreply.github.com>
2024-06-29 16:20:32 +0530

    Removed Unused Imports and Attached the Plan of Developing Projet

--- src/test/java/com/magicalArena/app/PlayerTest.java
+++ src/test/java/com/magicalArena/app/PlayerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.spy;
 
 @ExtendWith(MockitoExtension.class)
